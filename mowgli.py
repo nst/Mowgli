577a0ea166980782e859df53e0db545347fedf1e
Nicolas Seriot <nicolas@seriot.ch>
2024-08-11 08:21:54 +0200

    added viewport header

--- mowgli.py
+++ mowgli.py
@@ -23,6 +23,7 @@ template = """
     <link rel="stylesheet" href="$css">
     $extraheader
     <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 $body
